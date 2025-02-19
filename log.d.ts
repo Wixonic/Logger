d74ff93e35e2b1698ea86642ef902608887a5efc
Wixonic <contact@wixonic.fr>
2025-02-19 11:31:09 +0100

    Adding types

---
+++ log.d.ts
@@ -0,0 +1,35 @@
+declare module "@wixonic/logger" {
+	export interface Logger {
+		debug(...args: any[]): void;
+		error(...args: any[]): void;
+		info(...args: any[]): void;
+		warn(...args: any[]): void;
+	}
+
+	export const colors: {
+		reset: string;
+		bright: string;
+		dim: string;
+		underscore: string;
+		blink: string;
+		reverse: string;
+		hidden: string;
+		black: string;
+		red: string;
+		green: string;
+		yellow: string;
+		blue: string;
+		magenta: string;
+		cyan: string;
+		white: string;
+		debug: string;
+		error: string;
+		info: string;
+		warn: string;
+		regexp: RegExp;
+	};
+
+	export const log: Logger;
+
+	export function rawLog(level: string, color: string, ...args: any[]): void;
+};
